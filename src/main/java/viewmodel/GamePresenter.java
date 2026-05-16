59656c9454aadb380f9a57d6feca064ed310a134
Antoni <antoni.pierzchala06@gmail.com>
2026-05-16 14:50:24 +0200

    Dodałem wszytkie lotniska, których będziemy używać oraz rozpocząłem pracę nad view - zaimplementowałem rysowanie lotnisk jako kształtów oraz wyświetlanie mapy.

--- src/main/java/viewmodel/GamePresenter.java
+++ src/main/java/viewmodel/GamePresenter.java
@@ -1,14 +1,19 @@
 package viewmodel;
 
+import model.Airport;
 import model.BasicGameData;
+import java.util.List;
 
 public class GamePresenter {
     private BasicGameData data;
+    private List<Airport> airports;
 
     public GamePresenter() {
         this.data = new BasicGameData();
+        this.airports = AirportListGenerator.generateAirports();
     }
 
     public String getTitle() { return data.getTitle(); }
     public float[] getColor() { return data.getSkyColor(); }
+    public List<Airport> getAirports() { return airports; }
 }
